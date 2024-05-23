e8eb5f74993dea5176ebb31ef2b42b41d0db39ef
Cathleen Xi <cxi50@stuy.edu>
2024-05-23 10:44:40 -0400

    wrote constructor and started other methods

--- Game.java
+++ Game.java
@@ -6,7 +6,34 @@ public class Game{
     private int timer;
 
     public Game(){
-        
+        timer = 90; //seconds
+        avatar = new Avatar();
+        currTheme = 0;
+        themes.add("Emo");
+        themes.add("Academia");
+        themes.add("MissUniverse");
+        themes.add("OldMoney");
+        themes.add("Y2K");
+        themes.add("ApologyVideo");
+        themes.add("StreetStyle");
+        themes.add("Gym");
+        themes.add("Cozy");
+        themes.add("MeanGirl");
+
+
+
     }
+
+    public void newRound(){
+        currTheme ++;
+        timer = 90;
+        avatar = new Avatar();
+
+        // closet may be randomized here
+    }
+
+    public selectClothing(){}
+
+    public removeClothing(){}
 
 }
