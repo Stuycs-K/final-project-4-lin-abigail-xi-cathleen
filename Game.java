beacea062fdecc1b91b1f1dcec62f6c39a1e6ce9
Cathleen Xi <cxi50@stuy.edu>
2024-05-23 10:28:23 -0400

    started Game class

---
+++ Game.java
@@ -0,0 +1,12 @@
+public class Game{
+    private ArrayList<String> themes;
+    private int currTheme int;
+    private Avatar avatar;
+    private ArrayList<Clothing> closet;
+    private int timer;
+
+    public Game(){
+        
+    }
+
+}
